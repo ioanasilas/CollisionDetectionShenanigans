fbf0a9f0ddda027259ff895c80b4a4b10ec3a22e
Yossef <162422824+YossefUVT@users.noreply.github.com>
2025-01-30 00:38:19 +0200

    See you again

--- shared_data.py
+++ shared_data.py
@@ -3,8 +3,8 @@ times = []
 averages = {}
 
 # No. of Points Testing: Amount of Runs
-pointsTestingDict = {10: 100, 50: 50, 100: 20, 500: 10, 1000: 10, 10000: 5}
+pointsTestingDict = {10: 100, 50: 50, 100: 25, 500: 20, 1000: 15, 5000: 10, 10000: 5}
 
 # No. of Points Testing: Amount of positions 'n'
 # Make sure Points testing is the same as in pointsTestingDict
-overlap_positions_testing = {10: 1, 50: 2, 100: 4, 500: 32, 1000: 64, 10000: 625}
+overlap_positions_testing = {10: 1, 50: 2, 100: 4, 500: 32, 1000: 64, 5000: 144, 10000: 625}
